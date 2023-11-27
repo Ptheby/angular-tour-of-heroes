292eed045f4fdd5f4e115ac6ac304c2cc466d53c
Paul Theby <paultheby@gmail.com>
2023-11-27 17:29:28 -0600

    Through page 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Pipe } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+
 import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+import { HeroesComponent } from "./heroes/heroes.component";
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        NgFor,
+        HeroesComponent,
+        Pipe,
+
+
+    ]
 })
 export class AppModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,30 @@
+
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  standalone:true,
+  imports:[
+    NgFor,
+    NgIf,
+    FormsModule
+
+  ],
+
 })
 export class HeroesComponent {
-hero:Hero = {
-  id:1,
-  name:"Windstorm"
-};
+  heroes=HEROES;
+selectedHero?:Hero;
+
+onSelect(hero:Hero):void {
+this.selectedHero=hero;
+}
 }
